2e2159e7a8fde457dcc68d2724444c25f8eb5183
Francisco Rodriguez <frodriguezg34@gmail.com>
2025-07-20 20:21:18 +0000

    new code added

---
+++ hello-world.py
@@ -0,0 +1,15 @@
+print("Hello, great world")
+
+message = "hello again "
+print(message)
+
+symbol = "%"
+status = "ready"
+level_string = "100"
+level_int = 100
+
+full_message = level_string + symbol + " " + status
+print(full_message)
+
+full_message2 = str(level_int) + symbol + " " + status
+print(full_message2 + " printing the int value as string")
